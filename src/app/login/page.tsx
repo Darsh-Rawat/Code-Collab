ad1be15ca314a45bf311cb65ad69ccea7072102c
Darsh Rawat <rawatdarsh@gmail.com>
2025-05-20 20:14:59 +0530

    Updated Signup Page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,8 +9,25 @@ import {
     Stack,
     AbsoluteCenter,
     Box,
+    VStack,
+    Link,
+    Heading,
+    Text,
+    Tabs,
+    InputGroup,
+    Center,
+    Flex,
+    Grid,
+    GridItem,
+    Image,
+    Separator,
+    HStack,
+    Checkbox,
+
+
 } from "@chakra-ui/react"
-import { PasswordInput } from "@/components/ui/password-input"
+import { PasswordInput, PasswordStrengthMeter, } from "@/components/ui/password-input"
+import { CiMail } from "react-icons/ci";
 import { useState } from "react"
 
 
@@ -18,35 +35,87 @@ import { useState } from "react"
 const page = () => {
     const [value, setValue] = useState("")
     return (
-        <Box minH="100vh"
-            backgroundImage="url('/img.png')"
-            backgroundSize="cover"
-            backgroundPosition="center"
-            backgroundRepeat="no-repeat">
-            <AbsoluteCenter bg={"gray.50"} rounded={"2xl"} boxSize={"md"} p={8} boxShadow={"md"}>
-                <Fieldset.Root size="lg" maxW="md">
-
-                    <Fieldset.Content>
 
 
-                        <Field.Root>
-                            <Field.Label color={"#262d36"} fontWeight={"semibold"} fontSize={"md"} fontFamily={"body"} >Email address</Field.Label>
-                            <Input name="email" type="email" variant={"outline"}  css={{ "--focus-color": "colors.blue.400" }} bg={"white"} color={"black"} w={"sm"} />
-                        </Field.Root>
+        <Box
+            position="absolute"
+            top="50%"
+            left="50%"
+            transform="translate(-50%, -50%)"
+            // bg={useColorModeValue("gray.100", "gray.800")}
+            p={1}
+            rounded="2xl"
+            boxShadow="2xl"
+        >
+            <Grid
+                templateColumns={{ base: "1fr", md: "1fr 1fr" }}
+                maxW="800px"
+                borderRadius="2xl"
+                overflow="hidden"
+            >
+                {/* Left side image */}
+                <GridItem display={{ base: "none", md: "block" }}>
+                    <Image
+                        src="/Sign up-amico.svg"
+                        alt="Scenic"
+                        objectFit="contain"
+                        h="100%"
+                        w="100%"
+                    />
+                </GridItem>
 
+                {/* Right side form */}
+                <GridItem
+                    // bg={useColorModeValue("white", "gray.900")}
+                    // color={useColorModeValue("gray.800", "white")}
+                    p={8}
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                >
+                    <Box w="full" maxW="md">
+                        <Heading mb={2}>Create an account</Heading>
+                        <Text mb={6} fontSize="sm" color="gray.500">
+                            Already have an account?{" "}
+                            <Link color="purple.400" >
+                                Log in
+                            </Link>
+                        </Text>
 
-                        <Field.Root>
-                            <Field.Label color={"#262d36"} fontWeight={"semibold"} fontSize={"md"} fontFamily={"body"}>Password</Field.Label>
-                            <PasswordInput  css={{ "--focus-color": "colors.blue.400" }} bg={"white"} color={"black"} value={value} onChange={(e) => setValue(e.target.value)} />
-                        </Field.Root>
+                        <Stack gap={4}>
+                            <Grid templateColumns="1fr 1fr" gap={4}>
+                                <Input placeholder="First Name" />
+                                <Input placeholder="Last Name" />
+                            </Grid>
+                            <Input placeholder="Email" />
+                            <PasswordInput placeholder="Password" value={value} onChange={(e) => setValue(e.target.value)}></PasswordInput>
+                            <PasswordStrengthMeter value={2} />
+                            <Checkbox.Root>
+                                <Checkbox.HiddenInput />
+                                <Checkbox.Control />
+                                <Checkbox.Label>I agree to the terms & conditions</Checkbox.Label>
+                            </Checkbox.Root>
 
-                    </Fieldset.Content>
+                            <Button colorScheme="purple" w="full">
+                                Create account
+                            </Button>
 
-                    <Button type="submit" alignSelf="stretch" bg={"blue.400"} _hover={{ bg: "blue.500" }} color={"white"} fontWeight={"bolder"}>
-                        Sign in
-                    </Button>
-                </Fieldset.Root>
-            </AbsoluteCenter>
+                            <HStack>
+                                <Separator flex="1" />
+                                <Text textAlign="center" fontSize="sm" color="gray.500">
+                                    Or
+                                </Text>
+                                <Separator flex="1" />
+                            </HStack>
+
+                            <Grid templateColumns="1fr 1fr" gap={4}>
+                                <Button variant="outline">Google</Button>
+                                <Button variant="outline">Apple</Button>
+                            </Grid>
+                        </Stack>
+                    </Box>
+                </GridItem>
+            </Grid>
         </Box>
 
     )
